dd9646e579c4d9e3d7ed75f3c93daee9138ab938
DenZet <dimplat2002@mail.ru>
2023-02-08 21:25:56 +0300

    if else

---
+++ Example005_If_else/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("ВВедите имя ");
+string username = Console.ReadLine();
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура это Маша ");
+}
+else
+{
+    Console.WriteLine("Привет, " + username);
+}
