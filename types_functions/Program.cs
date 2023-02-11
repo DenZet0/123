1924f0ab622a15efffc4ed46ba448050c7074c49
DenZet <dimplat2002@mail.ru>
2023-02-11 23:46:09 +0300

    types

--- types_functions/Program.cs
+++ types_functions/Program.cs
@@ -40,17 +40,37 @@ int Method3()
 
 //Вид 4
 
+//string Method4(int count, string text)
+//{
+//    int i = 0;
+//    string result = String.Empty;
+//    while(i < count)
+//    {
+//       result = result + text;
+//        i++;
+//    }
+//    return result;
+//}
+
 string Method4(int count, string text)
 {
-    int i = 0;
     string result = String.Empty;
-    while(i < count)
+    for(int i = 0; i < 10; i++)
     {
-        result = result + text;
-        i++;
+       result = result + text;
     }
     return result;
 }
 
-string res = Method4(10, "asdf");
-Console.WriteLine(res);
+// string res = Method4(10, "asdf");
+// Console.WriteLine(res);
+
+
+for(int i = 1; i <= 10; i++ )
+{
+    for (int j = 1; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} * {j} = {i * j}");
+    }
+    Console.WriteLine();
+}
