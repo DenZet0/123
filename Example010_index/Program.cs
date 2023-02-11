27d0142298955de788467456f1dd9797fd82c8a9
DenZet <dimplat2002@mail.ru>
2023-02-11 19:39:39 +0300

    index

---
+++ Example010_index/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {10, 21, 23, 44, 15, 46, 87, 87};
+
+int n = array.Length;
+int find = 87;
+
+int index = 0;
+
+while(index < n) 
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
