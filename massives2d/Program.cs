37c47b130206e0811eff0c496c615a0a9d9b4a0b
DenZet <dimplat2002@mail.ru>
2023-02-18 22:59:20 +0300

    matrix

---
+++ massives2d/Program.cs
@@ -0,0 +1,44 @@
+﻿string[,] table = new string[2,5];
+
+//table[2,5]
+// 2 строки 5 столбцов
+
+// table[1, 2] = "Слово";
+
+// for (int rows = 0; rows < 2; rows++)
+// {
+//     for (int columns = 0; columns < 5; columns++)
+//     {
+//         Console.WriteLine($"-{table[rows, columns]}-");
+//     }
+// }
+
+
+void PrintArray(int[,] matr)
+{
+for (int row = 0; row < matr.GetLength(0); row++)
+{
+    for (int column = 0; column < matr.GetLength(1); column++)
+    {
+        Console.Write($"{matr[row, column]} ");
+    }
+    Console.WriteLine();
+}
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+int[,] matrix = new int[3, 4];
+PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
